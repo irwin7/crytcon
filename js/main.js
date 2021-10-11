80e3be27053506cf3b9e1a7e96603ee75832de8e
irwin7 <ravshankhan777@icloud.com>
2021-10-11 10:00:31 +0500

    site-header update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 let elMenuBtn = document.querySelector('.site-header__menu-div');
 let elMenuSpan= document.querySelector('.site-header__menu');
 let elNav = document.querySelector('.nav');
-let elNavItem = document.querySelector('.nav__item');
+let elNavItem = document.querySelector('.nav__item:nth-child(1)');
+let elNavItem2 = document.querySelector('.nav__item:nth-child(2)');
 let elPlusBtn = document.querySelector('.nav__item-plus');
+let elPlusBtn2 = document.querySelector('.nav__item-plus-sec');
 
 elMenuBtn.addEventListener('click' , function(){
   elNav.classList.toggle('open');
@@ -10,4 +12,7 @@ elMenuBtn.addEventListener('click' , function(){
 })
 elPlusBtn.addEventListener('click' , function(){
   elNavItem.classList.toggle('open-nav');
+})
+elPlusBtn2.addEventListener('click' , function(){
+  elNavItem2.classList.toggle('open-nav');
 })
